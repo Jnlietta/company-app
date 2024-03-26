a4081f495599b60d89d81a94351f8adffe2a2f36
Julia Glinka <julia.w.glinka@gmail.com>
2024-03-26 12:49:09 +0100

    Change codes of endpoints POST, PUT, DELETE in departments.routes

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -32,19 +32,39 @@ router.get('/departments/:id', (req, res) => {
 
 router.post('/departments', (req, res) => {
   const { name } = req.body;
-  db.departments.push({ id: 3, name })
-  res.json({ message: 'OK' });
+
+  req.db.collection('departments')
+    .insertOne({ name: name })
+    .then(() => {
+      res.json({ message: 'OK' });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    })
 });
 
 router.put('/departments/:id', (req, res) => {
   const { name } = req.body;
-  db = db.departments.map(item => (item.id == req.params.id) ? { ...item, name } : item );
-  res.json({ message: 'OK' });
+
+  req.db.collection('departments')
+    .updateOne({ _id: ObjectId(req.params.id) }, { $set: { name: name }})
+    .then(() => {
+      res.json({ message: 'OK' });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    })
 });
 
 router.delete('/departments/:id', (req, res) => {
-  db = db.departments.filter(item => item.id != req.params.id)
-  res.json({ message: 'OK' });
+  req.db.collection('departments')
+    .deleteOne({ _id: ObjectId(req.params.id) })
+    .then(() => {
+      res.json({ message: 'OK' });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    })
 });
 
 module.exports = router;
