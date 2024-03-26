5772b59bb9dbad0374093c92c83e68db0dfb090c
Julia Glinka <julia.w.glinka@gmail.com>
2024-03-26 11:59:08 +0100

    Change codes of endpoints GET and import method ObjectId in departments.routes

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./../db');
+const ObjectId = require('mongodb').ObjectId;
 
 router.get('/departments', (req, res) => {
-  res.json(db.departments);
+  req.db.collection('departments')
+    .find()
+    .toArray()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    });
 });
 
 router.get('/departments/random', (req, res) => {
@@ -11,7 +19,15 @@ router.get('/departments/random', (req, res) => {
 });
 
 router.get('/departments/:id', (req, res) => {
-  res.json(db.departments.find(item => item.id == req.params.id));
+  req.db.collection('departments')
+    .findOne({ _id: ObjectId(req.params.id) })
+    .then((data) => {
+      if(!data) res.status(404).json({ message: 'Not found' });
+      else res.json(data);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    });
 });
 
 router.post('/departments', (req, res) => {
