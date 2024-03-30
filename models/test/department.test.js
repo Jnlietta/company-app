1373d7db79e2e352d939b105c8aef1af276c45ce
Julia Glinka <julia.w.glinka@gmail.com>
2024-03-30 18:46:50 +0100

    Add test for department model

---
+++ models/test/department.test.js
@@ -0,0 +1,30 @@
+const Department = require('../department.model.js');
+const expect = require('chai').expect;
+const mongoose = require('mongoose');
+
+describe('Department', () => {
+    it('should throw an error if no "name" arg', async () => {
+        const dep = new Department({}); // create new Department, but don't set `name` attr value
+    
+      dep.validateSync(err => {
+        expect(err.errors.name).to.exist;
+      });  
+    });
+    
+    it('should throw an error if "name" is not a string', () => {
+
+        const cases = [{}, []];
+        for(let name of cases) {
+          const dep = new Department({ name });
+      
+          dep.validateSync(err => {
+            expect(err.errors.name).to.exist;
+          });
+        }
+    });  
+});
+
+
+after(() => {
+    mongoose.models = {};
+  });
