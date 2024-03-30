33a5c88349f40157758354d3646bd74cb7f7af25
Julia Glinka <julia.w.glinka@gmail.com>
2024-03-30 20:11:49 +0100

    Add two tests for employee model

---
+++ models/test/employee.test.js
@@ -0,0 +1,29 @@
+const Employee = require('../employee.model.js');
+const expect = require('chai').expect;
+const mongoose = require('mongoose');
+
+describe('Employee', () => {
+    it('should throw an error if no "firstName, lastName, department" arg', async () => {
+        const dep = new Employee({}); 
+    
+      dep.validateSync(err => {
+        expect(err.errors.name).to.exist;
+      });  
+    });
+
+    it('should throw an error if "firstName, lastName, department" is not a string', () => {
+
+        const cases = [{}, []];
+        for(let data of cases) {
+          const dep = new Employee({ data });
+      
+          dep.validateSync(err => {
+            expect(err.errors.data).to.exist;
+          });
+        }
+    });
+});
+
+after(() => {
+    mongoose.models = {};
+  });
