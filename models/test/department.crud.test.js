d7d1b85ced269b303b2b9b5675651005ca3082e3
Julia Glinka <julia.w.glinka@gmail.com>
2024-04-01 12:52:55 +0200

    Add before and connection to companyDBtest in department.crud.test

--- models/test/department.crud.test.js
+++ models/test/department.crud.test.js
@@ -1,10 +1,19 @@
 const Department = require('../department.model');
 const expect = require('chai').expect;
+const mongoose = require('mongoose');
 
 describe('Department', () => {
+    before(async () => {
 
-  after(() => {
-    mongoose.models = {};
-  });
+        try {
+          await mongoose.connect('mongodb://localhost:27017/companyDBtest', { useNewUrlParser: true, useUnifiedTopology: true });
+        } catch(err) {
+          console.error(err);
+        }
+    });
+
+    after(() => {
+        mongoose.models = {};
+    });
 
 });
