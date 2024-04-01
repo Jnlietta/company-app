3156a9ff43054d83cd42a5b165fa3ab2b9c2afaf
Julia Glinka <julia.w.glinka@gmail.com>
2024-04-01 14:56:52 +0200

    Add imports and connection to companyDBtest in employee.crud.test

---
+++ models/test/employee.crud.test.js
@@ -0,0 +1,14 @@
+const Employee = require('../employee.model');
+const expect = require('chai').expect;
+const mongoose = require('mongoose');
+
+describe('Employee', () => {
+    before(async () => {
+        try {
+          await mongoose.connect('mongodb://localhost:27017/companyDBtest', { useNewUrlParser: true, useUnifiedTopology: true });
+        } catch(err) {
+          console.error(err);
+        }
+    });
+    
+});
