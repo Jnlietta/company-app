8845282c31532c0fd1ba7b354818986657be4f45
Julia Glinka <julia.w.glinka@gmail.com>
2024-04-01 19:13:45 +0200

    Add before and after hooks inside departments GET method

--- test/api/departments/get.test.js
+++ test/api/departments/get.test.js
@@ -8,6 +8,17 @@ const expect = chai.expect;
 const request = chai.request;
 
 describe('GET /api/departments', () => {
+    before(async () => {
+        const testDepOne = new Department({ _id: '5d9f1140f10a81216cfd4408', name: 'Department #1' });
+        await testDepOne.save();
+      
+        const testDepTwo = new Department({ _id: '5d9f1159f81ce8d1ef2bee48', name: 'Department #2' });
+        await testDepTwo.save();
+    });
+      
+    after(async () => {
+        await Department.deleteMany();
+    });
 
     it('/ should return all departments', () => {
 
