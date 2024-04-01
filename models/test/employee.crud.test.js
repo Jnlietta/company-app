82f187bc3ff87963356d8af6ae4cebd24aaa067d
Julia Glinka <julia.w.glinka@gmail.com>
2024-04-01 15:17:31 +0200

    Add tests for READ group in employee.crud.test

--- models/test/employee.crud.test.js
+++ models/test/employee.crud.test.js
@@ -10,5 +10,38 @@ describe('Employee', () => {
           console.error(err);
         }
     });
-    
+
+    describe('Reading data', () => {
+        before(async () => {
+            const testEmpOne = new Employee({ firstName: 'firstName #1', lastName: 'lastName #1', department: 'department #1' });
+            await testEmpOne.save();
+
+            const testEmpTwo = new Employee({ firstName: 'firstName #2', lastName: 'lastName #2', department: 'department #2' });
+            await testEmpTwo.save();
+        });
+
+        it('should return all the data with "find" method', async () => {
+            const allEmployees = await Employee.find();
+            const expectedLength = 2;
+            expect(allEmployees.length).to.be.equal(expectedLength);
+        });
+
+        it('should return proper document by various params with "findOne" method', async () => {
+            const empByFirstName = await Employee.findOne({ firstName: 'firstName #1' });
+            const expectedFirstName = 'firstName #1';
+            expect(empByFirstName.firstName).to.be.equal(expectedFirstName);
+
+            const empByLastName = await Employee.findOne({ lastName: 'lastName #2' });
+            const expectedLastName = 'lastName #2';
+            expect(empByLastName.lastName).to.be.equal(expectedLastName);
+            
+            const empByDepartment = await Employee.findOne({ department: 'department #1' });
+            const expectedDepartment = 'department #1';
+            expect(empByDepartment.department).to.be.equal(expectedDepartment);
+        });
+
+        after(async () => {
+            await Employee.deleteMany();
+        });
+    });
 });
