e82e7a13c07d65bb528786fa00356ebe30553cdb
Julia Glinka <julia.w.glinka@gmail.com>
2024-04-01 18:50:01 +0200

    Add export/import of server

--- test/api/departments/get.test.js
+++ test/api/departments/get.test.js
@@ -1,5 +1,6 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const server = require('../../server.js');
 
 chai.use(chaiHttp);
 
@@ -8,4 +9,15 @@ const request = chai.request;
 
 describe('GET /api/departments', () => {
 
+    it('/ should return all departments', () => {
+
+    });
+  
+    it('/:id should return one department by :id ', () => {
+  
+    });
+  
+    it('/random should return one random department', () => {
+  
+    });
 });
