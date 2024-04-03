9b41f3f924bea25dc1729f54d8115adf661a4de0
Julia Glinka <julia.w.glinka@gmail.com>
2024-04-03 22:46:12 +0200

    Fix two tests for employee model - 30.2

--- models/test/employee.test.js
+++ models/test/employee.test.js
@@ -3,35 +3,26 @@ const expect = require('chai').expect;
 const mongoose = require('mongoose');
 
 describe('Employee', () => {
-    it('should throw an error if no "firstName, lastName, department" arg', async () => {
-        const dep = new Employee({}); 
-    
-      dep.validateSync(err => {
-        expect(err.errors.firstName).to.exist;
-        expect(err.errors.lastName).to.exist;
-        expect(err.errors.department).to.exist;
-      });  
-    });
+  it('should throw an error if no "firstName, lastName, department" arg', async () => {
+      const employee = new Employee({}); 
+      const error = employee.validateSync();
+      expect(error.errors.firstName).to.exist;
+      expect(error.errors.lastName).to.exist;
+      expect(error.errors.department).to.exist;
+  });
 
-    it('should throw an error if "firstName, lastName, department" is not a string', () => {
-
-        const cases = [{}, []];
-        for (let data of cases) {
-            const employee = new Employee({ 
-                firstName: data,
-                lastName: data,
-                department: data
-            });
-      
-          employee.validateSync(err => {
-            expect(err.errors.firstName).to.exist;
-            expect(err.errors.lastName).to.exist;
-            expect(err.errors.department).to.exist;
+  it('should throw an error if "firstName, lastName, department" is not a string', () => {
+      const cases = [{}, []];
+      for (let data of cases) {
+          const employee = new Employee({ 
+              firstName: data,
+              lastName: data,
+              department: data
           });
-        }
-    });
+          const error = employee.validateSync();
+          expect(error.errors.firstName).to.exist;
+          expect(error.errors.lastName).to.exist;
+          expect(error.errors.department).to.exist;
+      }
+  });
 });
-
-
-  //pytania do mentora 30.2 nie rozumiem jak ma działać ten test, niezależnie od tego
-  //co wpisze test jest zawsze pozytywny? dlaczego, skad mam pewnosc ze przyjelam sluszne zalozenie?
